32390522bc303f9295282dce72c05d904574a1d3
Kurt Mansperger <kurt.mansperger@gmail.com>
2024-05-09 21:15:13 -0400

    Fixed LinkedIn link in header

--- components/Navbars/IndexNavbar.js
+++ components/Navbars/IndexNavbar.js
@@ -38,6 +38,7 @@ export default function Navbar(props) {
             <li className="flex items-center">
               <a
                 className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                href="https://www.linkedin.com/in/kurtmansperger/"
                 target="_blank"
               >
                 <i className="text-blueGray-400 fab fa-linkedin text-lg leading-lg " />
